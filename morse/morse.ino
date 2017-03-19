f6aeae02bf4c4146c74afa83c08bbc0fd6bf93f5
Charlotte Dann <ask@pouretrebelle.com>
2017-03-19 18:10:25 +0000

    :art: Relayout circuit to know which output is being used, no more green LED

--- morse/morse.ino
+++ morse/morse.ino
@@ -2,17 +2,23 @@
 #include "QList.cpp"
 
 int PIN_COLOR_R = 12;
-int PIN_COLOR_G = 14;
 int PIN_COLOR_B = 13;
 
-int PIN_BUTTON = 4;
+int PIN_BUTTON = 16;
 bool buttonState = false;
 bool buttonActive = false;
 
 int dialRead = 0;
 int PIN_DIAL = A0;
 
-int PIN_BUZZER = 5;
+int PIN_PIEZO = 15;
+int PIN_MAGNET = 14;
+int PIN_OUTPUT;
+int PIN_TOGGLE_PIEZO = 4;
+int PIN_TOGGLE_MAGNET = 5;
+bool piezoActive = false;
+bool magnetActive = false;
+bool outputActive = false;
 int buzzSpeed = 20;
 
 QList<char*> incoming;
@@ -23,47 +29,64 @@ void setup(void) {
   messagesPending = true;
 
   pinMode(PIN_COLOR_R, OUTPUT);
-  pinMode(PIN_COLOR_G, OUTPUT);
   pinMode(PIN_COLOR_B, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
   pinMode(PIN_DIAL, INPUT);
-  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_MAGNET, OUTPUT);
+  pinMode(PIN_PIEZO, OUTPUT);
+  pinMode(PIN_TOGGLE_MAGNET, INPUT);
+  pinMode(PIN_TOGGLE_PIEZO, INPUT);
 }
 
 void loop(void) {
   checkStates();
 
+  if (!outputActive) {
+    return;
+  }
+
   if (messagesPending) {
     // set to red
     digitalWrite(PIN_COLOR_R, HIGH);
-    digitalWrite(PIN_COLOR_G, LOW);
     digitalWrite(PIN_COLOR_B, LOW);
   }
   else {
-    // set to green
+    // set to blue
     digitalWrite(PIN_COLOR_R, LOW);
-    digitalWrite(PIN_COLOR_G, HIGH);
-    digitalWrite(PIN_COLOR_B, LOW);
+    digitalWrite(PIN_COLOR_B, HIGH);
   }
 
   if (buttonActive) {
-    // set to blue
-    digitalWrite(PIN_COLOR_R, LOW);
-    digitalWrite(PIN_COLOR_G, LOW);
+    // set to pink
+    digitalWrite(PIN_COLOR_R, HIGH);
     digitalWrite(PIN_COLOR_B, HIGH);
 
     // wait a little after button activation
-    delay(500);
-    while (incoming.size() > 0) {
+    delay(200);
+
+    // loop through incoming, only if output is active
+    while (incoming.size() > 0 && outputActive) {
       outputString(morseEncode(incoming.front()));
-      incoming.pop_front();
+
+      if (outputActive) {
+        // only remove message if the output is still active
+        incoming.pop_front();
+      }
+
       // leave a space between messages
-      buzz(10, false);
+      if (incoming.size() > 0) {
+        buzz(10, false);
+      }
     }
-    messagesPending = false;
+
+    // if output is inactive there will still be messages pending
+    // because they won't have been poped
+    if (outputActive) {
+      messagesPending = false;
+    }
+
     buttonActive = false;
   }
-
 }
 
 void checkStates(void) {
@@ -75,7 +98,40 @@ void checkStates(void) {
   dialRead = analogRead(PIN_DIAL);
   buzzSpeed = round(map(dialRead, 11, 908, 30, 3));
 
-  if (buttonState != digitalRead(PIN_BUTTON)) {
+  // check the toggle switch status
+  piezoActive = digitalRead(PIN_TOGGLE_PIEZO);
+  magnetActive = digitalRead(PIN_TOGGLE_MAGNET);
+  bool outputActiveNext;
+  if (!piezoActive && !magnetActive) {
+    outputActiveNext = false;
+  }
+  else {
+    outputActiveNext = true;
+  }
+
+  // if the output has been turned on
+  if (!outputActive && outputActiveNext) {
+    // set the output appropriately
+    if (magnetActive) {
+      PIN_OUTPUT = PIN_MAGNET;
+    }
+    else {
+      PIN_OUTPUT = PIN_PIEZO;
+    }
+  }
+
+  // if the output has been turned off
+  if (!outputActiveNext && outputActive) {
+    // turn off light
+    digitalWrite(PIN_COLOR_R, LOW);
+    digitalWrite(PIN_COLOR_B, LOW);
+  }
+
+  // update output
+  outputActive = outputActiveNext;
+
+  if (buttonState != digitalRead(PIN_BUTTON)
+      && outputActive) {
     buttonState = digitalRead(PIN_BUTTON);
     if (buttonState == true) {
       buttonActive = !buttonActive;
@@ -91,9 +147,9 @@ void buzz(int length, bool active) {
   if (active) {
     for (int i = 0; i < loops; i++) {
       // 3 is enough for an okay square wave
-      digitalWrite(PIN_BUZZER, HIGH);
+      digitalWrite(PIN_OUTPUT, HIGH);
       delay(3);
-      digitalWrite(PIN_BUZZER, LOW);
+      digitalWrite(PIN_OUTPUT, LOW);
       delay(3);
     }
   }
@@ -104,7 +160,9 @@ void buzz(int length, bool active) {
 }
 
 void outputString(String morseMessage) {
-  for(int i = 0; i <= morseMessage.length(); i++) {
+  for (int i = 0; i <= morseMessage.length(); i++) {
+    if (!outputActive) return;
+
     switch(morseMessage[i]) {
       case '.': // dit
         buzz(1, true);
